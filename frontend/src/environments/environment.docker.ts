85b435aaa6be14a435ee815b7f235ab128934db1
Cleverton Inacio <cleverton.inacio@ufrn.br>
2026-03-11 23:38:35 +0000

    feat: init project

---
+++ frontend/src/environments/environment.docker.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://moni-backend:8080/api/v1', // URL do seu Backend Java
+};
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://moni-api-sua-url.com/api/v1',
+};
